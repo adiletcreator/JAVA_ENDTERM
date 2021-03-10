8e645e6f429bbd192d8a97836a763c9a657639b7
adiletcreator <201407@astanait.edu.kz>
2021-03-10 17:05:56 +0600

    Added project to github

--- src/kz/aitu/oop/practice/assignment5/data/interfaces/IDB.java
+++
@@ -1,8 +0,0 @@
-package kz.aitu.oop.practice.assignment5.data.interfaces;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface IDB {
-    Connection getConnection() throws SQLException, ClassNotFoundException;
-}
